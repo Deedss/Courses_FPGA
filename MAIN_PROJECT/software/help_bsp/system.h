9167a0456ab2a206282d61ac0febcdc7da05ee8e
sidoi <sidoiklumper@gmail.com>
2020-06-18 11:58:21 +0200

    👍😁🔫
    sd kaart reading

--- MAIN_PROJECT/software/help_bsp/system.h
+++ MAIN_PROJECT/software/help_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'nios'
  * SOPC Builder design path: ../../nios.sopcinfo
  *
- * Generated: Tue Jun 16 16:56:43 CEST 2020
+ * Generated: Thu Jun 18 11:41:41 CEST 2020
  */
 
 /*
